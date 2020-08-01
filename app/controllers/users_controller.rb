8a0459ae9abec2117b8e54a8e048ea451e1dae6d
Yuki-Ichigo <bzpl1215@gmail.com>
2020-08-02 00:40:33 +0900

    [途中]課題2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @user_books = @user.books
     @book = Book.new
   end
 
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
